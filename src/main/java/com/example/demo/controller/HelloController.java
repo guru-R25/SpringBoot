29c9a1f5dabdf13d84da9e54d3ede04042543fec
Manikandan Subramaniyan <manisubu3295@gmail.com>
2021-03-21 20:33:32 +0800

    test

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.dto.Student;
@@ -10,16 +11,17 @@ import com.example.demo.dto.Student;
 @RestController
 public class HelloController {
  
-    @RequestMapping("/hello")
-    public String hello() {
-        return "Hello World RESTful with Spring Boot";
-        
-       // Response.status(200).entity("helloworld").build();
-    }  
+	/*
+	 * @RequestMapping("/hello") public String hello() { return
+	 * "Hello World RESTful with Spring Boot";
+	 * 
+	 * // Response.status(200).entity("helloworld").build(); }
+	 */ 
     
-    @RequestMapping("/insertData")
-    public Student insertData() {
-    	Student student=null;
+    @RequestMapping(value ="/insertData",method = RequestMethod.POST, produces = "application/json")
+    public Student insertData(Student student) {
+    	//Student student=null;
+    	System.out.println("<--coming inside-->"+student);
     	return student;
     }
 }
--- src/main/java/com/example/demo/dto/Student.java
+++ src/main/java/com/example/demo/dto/Student.java
@@ -1,13 +1,22 @@
 package com.example.demo.dto;
 
+import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "sdudetails")
-public class Student {
+public class Student implements Serializable{
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
 	@Column(name = "id")
 	int id;
 	
@@ -25,6 +34,56 @@ public class Student {
 	
 	@Column(name = "gender")
 	String gender;
+	
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getStudentName() {
+		return studentName;
+	}
+
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+
+	public String getSection() {
+		return section;
+	}
+
+	public void setSection(String section) {
+		this.section = section;
+	}
+
+	public String getSubjects() {
+		return subjects;
+	}
+
+	public void setSubjects(String subjects) {
+		this.subjects = subjects;
+	}
+
+	public String getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(String dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
 	
 	
 
